aa4092cbfba1bd8e63c42c2847f256501684f40c
schorij23 <schorij23@gmail.com>
2024-01-13 17:18:12 -0500

    after tutor

--- src/jeffs-react-portfolio/src/App.jsx
+++ src/jeffs-react-portfolio/src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import Footer from './components/Footer/Footer.jsx'
+import Header from './components/Header/Header.jsx'
 
 function App() {
 
   return (
-   <h1>test</h1>
+    <>
+    <h1>test</h1>
+    <Header/>
+    <Footer/>
+   </>
   )
 }
 
---
+++ src/jeffs-react-portfolio/src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+
+  return (
+   <h1>this is the footer</h1>
+  )
+}
+
+export default Footer;
