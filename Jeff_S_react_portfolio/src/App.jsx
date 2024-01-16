a1990357d81fc500fce7c5823f633ef4823672fd
schorij23 <schorij23@gmail.com>
2024-01-15 23:10:42 -0500

    fixed

---
+++ Jeff_S_react_portfolio/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+function App() {
+
+
+  return (
+    <>
+    <h1>test</h1>
+    <Header/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ Jeff_S_react_portfolio/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+
+    return (
+        <h1>this is a footer</h1>
+    )
+}
+
+export default Footer;
