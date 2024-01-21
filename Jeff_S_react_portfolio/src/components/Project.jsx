33dc88bcbf3dc762a2e725b6c8636e4231742d9d
schorij23 <schorij23@gmail.com>
2024-01-21 11:20:22 -0500

    before tutor

---
+++ Jeff_S_react_portfolio/src/components/Project.jsx
@@ -0,0 +1,44 @@
+const projects = [
+    {
+        title: 'placeholder',
+        description: 'placeholder',
+        image: 'project 1',
+        url: 'https://.com',
+        github: 'https://github.com/'
+    },
+    {
+        title: 'placeholder',
+        description: 'placeholder',
+        image: 'project 2',
+        url: 'https://.com',
+        github: 'https://github.com/'
+    },
+    {
+        title: 'placeholder',
+        description: 'placeholder',
+        image: 'project 3',
+        url: 'https://.com',
+        github: 'https://github.com/'
+    },
+    {
+        title: 'placeholder',
+        description: 'placeholder',
+        image: 'project 4',
+        url: 'https://.com',
+        github: 'https://github.com/'
+    },
+    {
+        title: 'placeholder',
+        description: 'placeholder',
+        image: 'project 5',
+        url: 'https://.com',
+        github: 'https://github.com/'
+    },
+    {
+        title: 'placeholder',
+        description: 'placeholder',
+        image: 'project 6',
+        url: 'https://.com',
+        github: 'https://github.com/'
+    },
+]
