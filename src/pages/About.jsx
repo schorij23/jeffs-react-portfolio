75254042d3a0d6395d74e6a66e322e8cd3786481
schorij23 <schorij23@gmail.com>
2024-03-14 18:22:08 -0400

    better about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,13 +7,13 @@ export default function AboutPage() {
     <div className='container about'>
             <div className="row justify-content-md-center about_me">
                 <div className='about-me'>
-                    <img src={avatar} className='img-fluid' alt='JeffS-avatar' />
+                    <img src={avatar} className='img-fluid' width={{ width: '400px', height: '400px'}} alt='JeffS-avatar' />
                 </div>
             </div>
 
             <div className="col justify-content-md-center container-sm about-content">
                 <p className='about-body'>
-                    Hi, this is my portfolio. I am a web deveolper I have a degrees in Architectural Enginering and Construction
+                As a Full-Stack web developer from Pennsylvania, I specialize in front-end development, focusing on HTML, JavaScript, and design aesthetics. I take pride in a weather application that showcases national park weather, demonstrating my proficiency and dedication using multiple apis. Across various states, I’ve delivered innovative solutions, pushing the boundaries of web development. My passion lies in creating immersive digital experiences.
                 </p>
             </div>
         </div>
