99b4efef11c0016e2744136a096b8f04bfb75fd2
schorij23 <schorij23@gmail.com>
2025-03-23 09:52:21 -0400

    resume page cleanup

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -15,14 +15,17 @@ function Resume() {
        {/* Skills section */}
       <div className="col justify-content-md-center container-sm about-content" style={{ marginTop: '4%' , color: '#F5F5F5'}}>
             <p className='resume-front'>
-              Front End Skills include: HTML, JavaScript, React, CSS, Bootstrap, React Bootstrap, APIs
+              Front End Coding Skills and Knowledge include: HTML-5, CSS-3, JavaScript ES6+, Document Object Model
+              (DOM), Server-Side APIs and Third-Party APIs including Day.js and OpenWeather API 
+              <br></br><br /> Front End Frameworks include: React.js, jQuery, Express.js RestfulAPIS, Bootstrap, Tailwind, Pure CSS
             </p>
             <p className='resume-back'> 
-              Back End Skills include: SQL, Sequilize ORM, Model View Controler MVC and OOP
+              Back End Coding Skills and Knowledge include: SQL, Object Oriented Programing (OOP), 
+              and Object-Relational Mapping (ORM) 
+              <br></br><br /> Back End Frameworks include: Node.js Packages including Sequalize, MySQL2, Handlebars and Model View Controller (MVC) Architecture
             </p>
             <p className='resume-full'> 
-              Full Stack Skills include: node.js, Express, NoSql, React, Progressive Web Applications
-            </p>
+              Full Stack Coding and Knowledge Skills include: MERN stack (MongoDB (NoSQL), Express.js, React.js, Node.js), PWAs (Progressive Web Apps), Webpack, State and Redux            </p>
       </div>
   </div>
 );
